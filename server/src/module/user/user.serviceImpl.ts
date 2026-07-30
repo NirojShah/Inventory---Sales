5b9ce68ce13669fa2fa54ad607fc77bb8701640f
Niroj Shah <nirojshah1998@gmail.com>
2026-07-30 16:43:01 +0530

    Updated User service implementation.
    Use the creaetuserDto as the datatype.

--- server/src/module/user/user.serviceImpl.ts
+++ server/src/module/user/user.serviceImpl.ts
@@ -3,25 +3,26 @@ import UserService from "./user.service";
 import UserRepository from "./user.repository";
 import ResponseDto from "../../response/responsedto";
 import STATUS_CODES from "../../utility/status_codes";
+import { CreateUserDto } from "./user.model";
 
 class UserServiceImplementation implements UserService {
     private userRepository = new UserRepository();
 
-    async createUser(user: Prisma.UserCreateInput): Promise<ResponseDto> {
+    async createUser(user: CreateUserDto): Promise<ResponseDto> {
         const createdUser: User = await this.userRepository.createUser(user);
         return new ResponseDto(
-            STATUS_CODES.CREATED, 
-            "User created successfully.", 
-            {...createdUser, id: createdUser.id?.toString() as any}
+            STATUS_CODES.CREATED,
+            "User created successfully.",
+            { ...createdUser, id: createdUser.id?.toString() as any }
         );
     }
 
     async getUserById(id: number): Promise<ResponseDto> {
         const user: User = await this.userRepository.findById(id);
         return new ResponseDto(
-            STATUS_CODES.OK, 
-            "User found", 
-            {...user, id: user.id?.toString() as any}
+            STATUS_CODES.OK,
+            "User found",
+            { ...user, id: user.id?.toString() as any }
         );
     }
 }
